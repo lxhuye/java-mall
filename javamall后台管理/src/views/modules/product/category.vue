89e38831386dbe91dbe500ff75b953da14061fb8
Leaves <729809333@qq.com>
2020-08-26 15:18:15 +0800

    增加网关

---
+++ javamall后台管理/src/views/modules/product/category.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+</template>
+
+<script>
+  export default {
+    data(){
+        return{
+            data: [],
+            defaultProps: {
+                children: "children",
+                label: "label"
+            }
+        };
+    },
+    methods: {
+      handleNodeClick(data) {
+        console.log(data);
+      },
+      getMenus(){
+          this.$http({
+          url: this.$http.adornUrl('/product/category/list/tree'),
+          method: 'get',
+          params: this.$http.adornParams({
+            
+          })
+        }).then(data=>{
+            console.log("成功获取后台数据")
+        })
+      }
+    },
+    created(){
+        this.getMenus();
+    }
+  };
+</script>
+<style>
+</style>
